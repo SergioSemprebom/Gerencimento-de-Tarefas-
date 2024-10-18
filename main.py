fd59ed13bd6d4c1aeb7bf51794674d585147216e
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-10-18 00:19:26 -0300

    salvando o projeto App gerencidor de projeto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import flet as ft
+import flet as ft # type: ignore
+import sqlite3
 
 class ToDo:
     def __init__(self, page: ft.Page):
@@ -9,33 +10,112 @@ class ToDo:
         self.page.window_resizable = False
         self.page.window_always_on_top = True
         self.page.title = "ToDo App"
+        self.dbt_execute("CREATE TABLE IF NOT EXISTS tasks (name, status)")
         self.main_page()
+
+    def dbt_execute(self, query, params = []):
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute(query, params)
+            con.commit()
+            return cur.fetchall()
+
+    def checked(self, e):
+        is_checked = e.control.value
+        label = e.control.label
+
+        if is_checked:
+            self.dbt_execute("UPDATE tasks SET status = 'complete' WHERE name = ?", params=[label])
+        else:
+            self.dbt_execute("UPDATE tasks SET status = 'incomplete' WHERE name = ?", params=[label])
+
+
+        if self.vew == 'all':
+            self.results = self.dbt_execute("SELECT * FROM tasks")
+        else:
+            self.results = self.dbt_execute("SELECT * FROM tasks WHERE status = ?", params=[self.view])
+        self.update_task_list()
+
 
     def task_container(self):
         return ft.Container(
             height=self.page.height * 0.8,
-            content=ft.Column(
-                control=[
-                    ft.Checkbox(label='Tarefa 1', value=True)]))
+            content = ft.Column(
+                controls = [
+                    ft.Checkbox(label=res[0], 
+                                on_change = self.chacked
+                                value=True, == 'complete' else False)
+                    for res in self.results if res
+                ]
+            )
+        )
+    
+    def set_value(self, e):
+        self.task = e.control.value
+        print(self.task)
+
+
+
+    def add(self, e, input_task):
+        name = self.task
+        status = 'incomplete'
+
+        if name:
+            self.dbt_execute("INSERT INTO tasks VALUES (?, ?)",params=[name, status])
+            input_task.value = ''
+            self.results = self.dbt_execute("SELECT * FROM tasks")
+            self.update_task_list()
+           
+    def update_task_list(self):
+        self.tasks_container()
+
+
+    def tabs_changed(self, e):
+        if e.control.selected_index == 0:
+            self.results = self.dbt_execute("SELECT * FROM tasks")
+            self.view = 'all'
+        elif e.control.selected_index == 1:
+            self.results.db_execute("SELECT * FROM tasks WHERE status = 'incomplete'")
+            self.view = 'incomplete'
+        elif e.control.select_index == 2:
+            self.results = self.dbt_execute("SELECT * FROM tasks WHERE status = 'complete'")
+            self.view = 'complete'
+        self.update_task_list()
+
+
 
 
     def main_page(self):
-        input_task = ft.TextField(hint_text = " Digite uma tarefa", expand = True)
+        input_task = ft.TextField(hint_text = " Digite uma tarefa",
+                                   expand = True,
+                                   on_change=self.set_value
+                                )
 
         input_bar = ft.Row(
             controls = [
                 input_task,
-                ft.FloatingActionButton(icon = ft.icons.ADD)])
+                ft.FloatingActionButton(
+                    icon = ft.icons.ADD,
+                    on_click = lambda _: self.dbt_execute(
+                        [input_task.value, False]
+                    )
+
+                )
+            ]
+        )
         
         tabs = ft.Tabs(
             selected_index = 0,
-            tabs = [
+            on_change = self.tabs_changed,
+            tabs=[
                 ft.Tab(text='Todos'),
                 ft.Tab(text='Em andamento'),
-                ft.Tab(text='Finalizados')])
-        
+                ft.Tab(text='Finalizados')
+            ]
+        )
         
         tasks = self.task_container()
+
         self.page.add(input_bar, tabs, tasks)
 
 ft.app(target=ToDo)
